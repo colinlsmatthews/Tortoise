6f2941c620bab5916b64ae36864843328d9647af
Colin Matthews <matthews.colin@proton.me>
2024-01-22 16:39:57 -0500

    Add project files.

---
+++ EnneadTabForGHInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace EnneadTabForGH
+{
+    public class EnneadTabForGHInfo : GH_AssemblyInfo
+    {
+        public override string Name => "EnneadTab";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("2b29cd31-185a-4b51-8815-5e294364c767");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
