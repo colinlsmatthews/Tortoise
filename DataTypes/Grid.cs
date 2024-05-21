5b814de6f92df6d1362243ed7c786099886dd80e
Colin Matthews <matthews.colin@proton.me>
2024-05-21 01:15:54 -0400

    work on grid data type - in progress

--- DataTypes/Grid.cs
+++ DataTypes/Grid.cs
@@ -7,37 +7,57 @@ using System.Threading.Tasks;
 using Grasshopper;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Data;
 using Rhino;
 using Rhino.Geometry;
+using Grasshopper.Getters;
 
 namespace Tortoise.DataTypes
 {
-    internal class Grid : GH_Goo<DataTree<Object>>
+    internal class Grid : GH_Goo<GH_Structure<IGH_Goo>>
     {
-        // PROPERTIES
-        public DataTree<LineCurve> GridCurves { get; set; }
-        public DataTree<string> GridNames { get; set; }
-        public bool Flip { get; set; }
+        // PROPERTIES**************************************************
+        public GH_Structure<GH_Curve> GridCurves { get; set; }
+        public GH_Structure<GH_String> GridNames { get; set; }
+        public GH_Structure<GH_Boolean> Flip { get; set; }
 
 
-        // CONSTRUCTORS
+        // CONSTRUCTORS************************************************
         // Default constructor
         public Grid()
         {
-            GridCurves = new DataTree<LineCurve>();
-            GridNames = new DataTree<string>();
-            Flip = false;
+            GridCurves = new GH_Structure<GH_Curve>();
+            GridNames = new GH_Structure<GH_String>();
+            Flip = new GH_Structure<GH_Boolean>();
         }
 
-        // Add constructor overloads here
+        // Standard constructor
         public Grid(
-            DataTree<LineCurve> inputCurves,
-            DataTree<string> inputNames,
-            bool inputFlip)
+            GH_Structure<GH_Curve> inputCurves,
+            GH_Structure<GH_String> inputNames,
+            GH_Structure<GH_Boolean> inputFlip)
         {
             GridCurves = inputCurves;
             GridNames = inputNames;
             Flip = inputFlip;
+        }
+
+        // Nameless constructor w/ flip
+        public Grid(
+            GH_Structure<GH_Curve> inputCurves,
+            GH_Structure<GH_Boolean> inputFlip)
+        {
+            GridCurves = inputCurves;
+            GridNames = autoName(); // Add logic to auto name
+            Flip = inputFlip;
+        }
+
+        // Nameless constructor w/out flip
+        public Grid(
+            GH_Structure<GH_Curve> inputCurves)
+        {
+            GridCurves = inputCurves;
+            GridNames = autoName();
         }
 
         // Copy constructor
@@ -54,26 +74,36 @@ namespace Tortoise.DataTypes
             return new Grid(this);
         }
 
-        // MEMBER OVERRIDES
+        // MEMBER OVERRIDES********************************************
         // Override properties inherited from IGH_Goo here
-        public override DataTree<Object> Value
+        public override GH_Structure<IGH_Goo> Value
         {
             get { return base.Value; }
             set
             {
-                DataTree<Object> dataTree = new DataTree<Object>();
-
+                GH_Structure<IGH_Goo> dataTree = new GH_Structure<IGH_Goo>();
             }
         }
 
         public override string ToString()
         {
-            return Value.ToString();
+            return
+                $"Structural grid with {numOrientations()} orientations." +
+                $"\nOrientation";
+                  
+            // Structural grid with {numOrientations()} orientations.
+            // Orientation {{0;0}[0].name}-{{0;0}[-1].name} (i.e. A-F) has {n} grid lines.
+            // Orientaiton {{0;1}[0].name}-{{0;0}[-1].name} (i.e. 1-9) has {n} grid lines.
+            // etc...
+            // Total number of grid lines: {linesTotal}
+            // Total number of structural bays: {baysTotal}
+            // Total number of grid intersections: {intersections}
+            // 
         }
 
         public override bool IsValid
         {
-            get { return true; }
+            get { return !GridCurves.IsEmpty; }
         }
 
         public override string TypeName
@@ -82,8 +112,48 @@ namespace Tortoise.DataTypes
         }
         public override string TypeDescription
         {
-            get { return "Grid"; }
+            get { return "Structural grid object"; }
         }
+
+        // METHODS*****************************************************
+        private int numOrientations()
+        {
+            // Calculate number of orientations (num of paths)
+            return GridCurves.Branches.Count;
+        }
+
+        private int numLines()
+        {
+            // Calculate number of structural lines
+            List<GH_Curve> flattenedData = GridCurves.FlattenData();
+            return flattenedData.Count;
+            return 0;
+        }
+
+        private int numBays()
+        {
+            // Calculate number of strutural bays
+            return 0;
+        }
+
+        private int numIntersections()
+        {
+            // Calculate number of grid intersections
+            return 0;
+        }
+
+        private GH_Structure<GH_String> autoName()
+        {
+            // Auto generate grid names
+            // Deconstruct GridCurves paths
+            // {0;0} - A,B,C,etc...
+            // {0;1} - 1,2,3,etc...
+            // {0;2} - AA,BB,CC,etc...
+            // {0;3} - 101,102,103,etc...
+            return new GH_Structure<GH_String>();
+        }
+
+
 
 
     }
